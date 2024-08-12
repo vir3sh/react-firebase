710b49c1bc728f1de5139728ffcd68df59a84ccf
vir3sh <pviresh508@gmail.com>
2024-08-12 22:14:32 +0530

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './login.css'
+
+function Login() {
+  return (
+    <div>
+      <div className="container">
+        <div className="form">
+        <h2>Login</h2>
+        <label htmlFor="Username">Username</label>
+        <input type="text" />
+        <label htmlFor="Password">Password</label>
+        <input type="password" />
+        <input type="button" displa />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
